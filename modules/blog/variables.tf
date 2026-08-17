7025350ab621c280f565987417772020a2e04342
Blake Cash <Blake.Cash@wizards.com>
2026-08-17 15:19:41 -0700

    Replace retired Bitnami Tomcat AMI with Canonical Ubuntu 24.04

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -12,8 +12,8 @@ variable "ami_filter" {
   })
 
   default = {
-    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-    owner = "979382823631" # Bitnami
+    name  = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
+    owner = "099720109477" # Canonical
   }
 }
 
